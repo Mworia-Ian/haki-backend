777f8bc58ade77911b3774d84c81bbf126526935
ianmmworia <ianmworia156@gmail.com>
2024-08-01 13:32:51 +0300

    to handle all the initial flask initialization

--- config.py
+++ config.py
@@ -1,1 +1,15 @@
 from flask import Flask
+from flask_cors import CORS
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate 
+
+
+app = Flask(__name__)
+
+CORS(app)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://postgres:123456@localhost/bookmate"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
