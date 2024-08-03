adc4bbaaac1a2c58b608814318db857accd5f9d1
CharlesKibet <charlesbiegon973@gmail.com>
2024-08-03 10:36:24 +0300

    Supabase connection

--- app.py
+++ app.py
@@ -5,12 +5,13 @@ from flask_cors import CORS
 from flask_restful import Api
 
 
-from models import db,User
+from models import db, User
 
 app = Flask(__name__)
 api = Api(app)
 
 
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres.ubtabihyrjnwkxhztihb:123!hakiapp@aws-0-ap-south-1.pooler.supabase.com:6543/postgres'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///haki.db'
 app.config['SQLALCHEMY_ECHO'] = True
 
@@ -24,9 +25,10 @@ db.init_app(app)
 @app.route('/')
 def home():
     return 'Hello from flask'
-  
-@app.route('/users',methods=['GET'])
+
+
+@app.route('/users', methods=['GET'])
 def users():
-     user = User.query.all()
-     print(user)
-     return []
+    user = User.query.all()
+    print(user)
+    return []
