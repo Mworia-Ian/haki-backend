562ffbdae8e9141564b6c7d15000ee5892788ff1
CharlesKibet <charlesbiegon973@gmail.com>
2024-08-05 14:13:22 +0300

    Main branch updated and JWT added

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from flask_bcrypt import Bcrypt
 from flask_cors import CORS
 from flask_migrate import Migrate
 from flask_restful import Api, Resource
+from flask_jwt_extended import JWTManager
 
 
 
@@ -16,7 +17,8 @@ app = Flask(__name__)
 api = Api(app)
 
 # Configure the app
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres.ubtabihyrjnwkxhztihb:123!hakiapp@aws-0-ap-south-1.pooler.supabase.com:6543/postgres"
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///haki.db'
+# app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres.ubtabihyrjnwkxhztihb:123!hakiapp@aws-0-ap-south-1.pooler.supabase.com:6543/postgres"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
 app.config['JWT_SECRET_KEY'] = "haki_secret_key"
@@ -28,7 +30,10 @@ db.init_app(app)
 migrate = Migrate(app, db, render_as_batch=True)
 bcrypt = Bcrypt(app)
 
+# setup jwt
+jwt = JWTManager(app)
 @app.route('/')
+
 def home():
     return 'Hello from Flask'
 
