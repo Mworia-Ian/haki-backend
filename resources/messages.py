284186920a140f96a369581b3bcfe347b365b8bf
Denis <mugambimuthuri0@gmail.com>
2024-08-12 11:46:41 +0300

    "Create MessageResource class skeleton with basic structure"

--- resources/messages.py
+++ resources/messages.py
@@ -1,4 +1,20 @@
 from firebase_admin import firestore
 
+from flask_restful import Resource
+from flask_jwt_extended import jwt_required, get_jwt_identity
+
 # Initialize Firestore
 db_firestore = firestore.client()
+
+class MessageResource(Resource):
+    @jwt_required()
+    def post(self):
+        pass
+
+    @jwt_required()
+    def get(self):
+        pass
+
+    @jwt_required()
+    def delete(self, message_id):
+        pass
