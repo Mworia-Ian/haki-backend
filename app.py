49bc650669764b902a596cf28b9ad554eddd3070
CharlesKibet <charlesbiegon973@gmail.com>
2024-08-19 14:30:48 +0300

    Minor changes

--- app.py
+++ app.py
@@ -46,34 +46,13 @@ bcrypt = Bcrypt(app)
 jwt = JWTManager(app)
 
 
-# Middleware for subscription check
-# def subscription_required(fn):
-#     @jwt_required()
-#     def wrapper(*args, **kwargs):
-#         user_id = get_jwt_identity()
 
-#         # Check for active subscription
-#         active_subscription = Subscription.query.filter_by(user_id=user_id, payment_status='Paid').order_by(Subscription.end_date.desc()).first()
-
-#         if not active_subscription or active_subscription.end_date < datetime.now():
-            
-#             return redirect(url_for('paymentresource'))
-
-#         return fn(*args, **kwargs)
-    
-#     return wrapper
-
-# HelloWorld resource
 
 class HelloWorld(Resource):
     def get(self):
         return {"message": "Hello Haki"}
 
 
-# class ProtectedResource(Resource):
-#     @subscription_required
-#     def get(self):
-#         return {"message": "You have access to this protected resource because you have an active subscription!"}
 
 
 api.add_resource(HelloWorld, '/')
@@ -94,7 +73,6 @@ api.add_resource(LawyerResource, '/lawyers', '/lawyers/<int:id>')
 api.add_resource(StkPush, '/stk_push')
 api.add_resource(SubscriptionResource, '/subscription')
 api.add_resource(PaymentStatusResource, '/payment_status/<string:transaction_id>')
-# api.add_resource(ProtectedResource, '/protected')  
 
 if __name__ == '__main__':
     app.run(debug=True)
